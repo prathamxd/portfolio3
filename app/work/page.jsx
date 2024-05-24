66d326388f66876f203d9e09c8ac0cdbf476c475
prathamxd <prathamkl17@gmail.com>
2024-05-24 19:41:19 +0530

    mono

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -7,9 +7,9 @@ const Work = () => {
     <div className="space-y-32 my-28">
       <div className="pl-28 space-y-7">
         <p className="text-6xl font-bold">Work.</p>
-        <p className="text-3xl w-[35%]">
+        <p className="text-3xl w-[35%] font-mono">
           Here are some of my{" "}
-          <span className="text-theme-primary-accentColor font-mono">projects</span> from
+          <span className="text-theme-primary-accentColor">projects</span> from
           the last few months.
         </p>
       </div>
